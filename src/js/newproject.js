527ea89f9e337a1cefe092799de028e01af856dd
Oscar G. <oscargomesribeiro@gmail.com>
2025-10-27 19:30:31 -0300

    Update newproject.js

--- src/js/newproject.js
+++ src/js/newproject.js
@@ -34,7 +34,6 @@ document.addEventListener('DOMContentLoaded', () => {
       const { user } = await getUser();
       if (user && membersInput) {
         loggedUserEmail = user.email;
-        membersInput.value = loggedUserEmail;
       }
     } catch (err) {
       console.error('Erro ao buscar usuário logado:', err);
